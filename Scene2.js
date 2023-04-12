cee12b8a3f6459aad4444ca071ac0ba933ae69b8
bracko-3 <cummijb@mail.uc.edu>
2023-04-11 23:54:51 -0400

    pipes work

--- Scene2.js
+++ Scene2.js
@@ -7,23 +7,69 @@ class Scene2 extends Phaser.Scene {
         this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
         this.background.setOrigin(0, 0);
         this.background.displayWidth = 600;
-        this.background.displayHeight = 700;
+        this.background.displayHeight = 500;
 
-        this.player = this.physics.add.sprite(config.width/2 - 100, config.height/2, "player");
+        this.player = this.physics.add.sprite(config.width/2 - 200, config.height/2, "player");
         this.player.angle += 90;
         this.player.play("thrust");
         this.cursorKeys = this.input.keyboard.createCursorKeys();
         this.player.setCollideWorldBounds(true);
 
-        this.pipes = [];
-        this.pipes.push(this.pipeObject(200));
-        this.pipes.push(this.pipeObject(400));
+        const pipeDistance = 50;
+        const height1 = 100 + Math.random() * 300;
+        const height2 = 100 + Math.random() * 300;
+        const height3 = 100 + Math.random() * 300;
+        const pipeOneDistance = 300;
+        const pipeTwoDistance = 500;
+        const pipeThreeDistance = 700;
+
+        //------------------Pipe Row 1------------------------
+        //Top Pipe
+        this.pipe1 = this.add.sprite(pipeOneDistance, height1 + pipeDistance, "pipe");
+        this.pipe1.displayOriginX = 0;
+        this.pipe1.displayOriginY = 0;
+
+        //Bottom Pipe
+        this.pipe2 = this.add.sprite(pipeOneDistance, height1 - pipeDistance, "pipe");
+        this.pipe2.displayOriginX = 0;
+        this.pipe2.displayOriginY = 0;
+        this.pipe2.displayHeight = -1 * this.pipe2.height;
+
+        //------------------Pipe row 2------------------------
+        //Top Pipe
+        this.pipe3 = this.add.sprite(pipeTwoDistance, height2 + pipeDistance, "pipe");
+        this.pipe3.displayOriginX = 0;
+        this.pipe3.displayOriginY = 0;
+
+        //Bottom Pipe
+        this.pipe4 = this.add.sprite(pipeTwoDistance, height2 - pipeDistance, "pipe");
+        this.pipe4.displayOriginX = 0;
+        this.pipe4.displayOriginY = 0;
+        this.pipe4.displayHeight = -1 * this.pipe2.height;
+
+        //------------------Pipe row 3------------------------
+        //Random Height
+        //Top Pipe
+        this.pipe5 = this.add.sprite(pipeThreeDistance, height3 + pipeDistance, "pipe");
+        this.pipe5.displayOriginX = 0;
+        this.pipe5.displayOriginY = 0;
+
+        //Bottom Pipe
+        this.pipe6 = this.add.sprite(pipeThreeDistance, height3 - pipeDistance, "pipe");
+        this.pipe6.displayOriginX = 0;
+        this.pipe6.displayOriginY = 0;
+        this.pipe6.displayHeight = -1 * this.pipe2.height;
     }
 
     update() {
         this.movePlayerManager();
 
-        this.movePipe(this.pipes);
+        this.movePipe(this.pipe1);
+        this.movePipe(this.pipe2);
+        this.movePipe(this.pipe3);
+        this.movePipe(this.pipe4);
+        this.movePipe(this.pipe5);
+        this.movePipe(this.pipe6);
     }
 
     movePlayerManager() {
@@ -35,23 +81,7 @@ class Scene2 extends Phaser.Scene {
         }
     }
 
-    pipeObject(x) {
-        //Random Height
-        const height = 100 + Math.random() * 400;
-
-        //Top Pipe
-        this.pipe1 = this.add.sprite(x, height + 50, "pipe");
-        this.pipe1.displayOriginX = 0;
-        this.pipe1.displayOriginY = 0;
-
-        //Bottom Pipe
-        this.pipe2 = this.add.sprite(x, height - 50, "pipe");
-        this.pipe2.displayOriginX = 0;
-        this.pipe2.displayOriginY = 0;
-        this.pipe2.displayHeight = -1 * this.pipe2.height;
-    }
-
     movePipe(pipe) {
-        pipe.x += -20;
+        pipe.x += -1;
     }
 }
--- game.js
+++ game.js
@@ -4,7 +4,7 @@ var gameSettings = {
 
 var config = {
 	width: 600,
-	height: 700,
+	height: 500,
 	backgroundColor: 0x000000,
 	scene: [Scene1, Scene2],
 	pixelArt: true,
